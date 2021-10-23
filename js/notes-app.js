5b76cee5bbdfa3d5689ea66b1fcab32d3baa37d5
Mohamed <qotozer@gmail.com>
2021-10-23 15:15:52 +0200

    adding features (filtering , adding) notes

--- js/notes-app.js
+++ js/notes-app.js
@@ -1,2 +1,26 @@
 
+
 let notes = settAndGetNotes()
+let filterText = {
+    'searchText':'',
+}
+
+renderNotes(notes,filterText)
+
+document.querySelector('#search-input').addEventListener('input',(e)=>{
+
+    filterText.searchText = e.target.value
+    renderNotes(notes,filterText)
+    
+})
+
+document.querySelector('#add-notes').addEventListener('click',(e)=>{
+    const id = uuidv4()
+    notes.push({
+        'id':id,
+        'title':'',
+        'body':'',
+    })
+    saveNotes()
+    location.assign('edite-note.html')
+})
--- js/notes-functions.js
+++ js/notes-functions.js
@@ -1,20 +1,45 @@
-const settAndGetNotes = ()=>{
-    
-    const notesJSON =localStorage.getItem('notes')
-    if(notesJSON!==null)
-    {
-        return JSON.parse(notesJSON)
-    }
-    else{
-        return []
-    }
+const notesDiv = document.querySelector("#notes");
+const settAndGetNotes = () => {
+  const notesJSON = localStorage.getItem("notes");
+  if (notesJSON !== null) {
+    return JSON.parse(notesJSON);
+  } else {
+    return [];
+  }
+};
 
+const removeNote = (noteID)=>{
+ const noteIndex= notes.findIndex((note)=>note.id===noteID)
+ notes.splice(noteIndex,1)
+ saveNotes()
+ renderNotes(notes,filterText)
+}
+const generateNotDom = (note)=>{
+   const noteEl= document.createElement('div')
+   const textEl=document.createElement('a')
+   const buttonEl = document.createElement('button')
+
+   textEl.textContent = note.title
+   textEl.setAttribute('href','../edite-note.html')
+   buttonEl.textContent = 'x'
+   buttonEl.addEventListener('click',(e)=>{
+    removeNote(note.id)
+   })
+   noteEl.appendChild(buttonEl)
+   noteEl.appendChild(textEl)
+   return noteEl
 }
 
-const saveNotes=()=>{
-    localStorage.setItem('notes',JSON.stringify(notes))
-}
-
-
-
-
+const saveNotes = () => {
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+const renderNotes = (notes, filterText) => {
+  let filteredNotes = notes.filter((element) =>
+    element.title.toLowerCase().includes(filterText.searchText)
+  );
+  notesDiv.innerHTML =""
+  filteredNotes.forEach((element) => {
+    const noteEl = generateNotDom(element)
+    notesDiv.appendChild(noteEl);
+  });
+};
